72372304a8aac5d04c72b7dea596e8a3c8b9a525
Mauro Filho <mauro.filho@gmail.com>
2024-11-09 18:04:06 -0300

    Initial commit

---
+++ src/test/java/com/filhodomauro/java_ci_test/JavaCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filhodomauro.java_ci_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
